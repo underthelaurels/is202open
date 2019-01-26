309c7a4ec159425c0bc223172431bb3986be5393
Mary Haninger <maryh15@cs.ct.edu>
2019-01-26 12:38:22 -0500

    confiq

---
+++ App/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// path constants
+define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
+define('BASE_URL','ec2-18-221-208-209.us-east-2.compute.amazonaws.com'); # your base URL
+
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_HOST', 'localhost');
