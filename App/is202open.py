3c6d6e58e3e297bbfc856ced16bdc2d32e854ed7
Laurel Romoser <laurelr@spruce.rlogin>
2019-01-25 21:38:54 -0500

    added the basic python file that outlines our logic

---
+++ App/is202open.py
@@ -0,0 +1,17 @@
+'''Is 202 open? Lets find out'''
+
+#create list of classes in 202 - list of touples?
+
+#store current DotW/time
+
+#loop through the list of classes
+    #if the current date is after the last day of classes
+        #remove the class from the list
+        #continue
+    #if the current DotW = class DotW
+        #is the curr time in between the start and stop time?
+            #return "no"
+
+#end of the loop, return "yes"
+
+#call the function, and print the result
