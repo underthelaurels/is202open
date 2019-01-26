d0d3f7ba2198cb6b54f5fd68e95b46c3097c9c74
Laurel Romoser <laurelr@pawpaw.rlogin>
2019-01-25 22:32:40 -0500

    im

--- App/is202open.py
+++ App/is202open.py
@@ -1,17 +1,34 @@
 '''Is 202 open? Lets find out'''
+import datetime
 
 #create list of classes in 202 - list of touples?
+#touple - (weekday, startTime, endTime, lastDay)
+classes = [(1, datetime.time(hour=10, minute=10), datetime.time(hour=12, minute=45), datetime.date(2019, 5, 8)),
+(1, datetime.time(hour=13, minute=25), datetime.time(hour=14, minute=15), datetime.date(2019, 5, 8)),
+(3, datetime.time(hour=10, minute=10), datetime.time(hour=12, minute=45), datetime.date(2019, 5, 8)),
+(3, datetime.time(hour=13, minute=25), datetime.time(hour=14, minute=15), datetime.date(2019, 5, 8))]
 
-#store current DotW/time
+def is202Open():
+    #store current DotW/Time
+    currDotW = datetime.datetime.today().weekday()
+    currTime = datetime.datetime.now().time()
 
-#loop through the list of classes
-    #if the current date is after the last day of classes
-        #remove the class from the list
-        #continue
-    #if the current DotW = class DotW
-        #is the curr time in between the start and stop time?
-            #return "no"
+    #loop through the list of classes
+    for class in classes:
+        #if the current date is after the last day of classes
+        if datetime.datetime.today() > class[3]:
+            #remove the class from the list/continue
+            continue
 
-#end of the loop, return "yes"
+        #if the current day of the week is the class day of the week
+        if currDotW = class[0]:
+            #is the curr time in between the start and stop time?
+            if currTime >= class[1] and currTime <= class[2]:
+                return "no"
+
+    #end of the loop, return "yes"
+    return "yes"
 
 #call the function, and print the result
+print("Is 202 open now?")
+print(is202Open())
